1751c3713832be5dea729c11a9269203a9a32c86
musgar <abungsalman@gmail.com>
2019-06-17 17:21:08 +0700

    Initial Solution Setup

---
+++ FinTax/FinTax.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinTax.WebUI.Startup))]
+namespace FinTax.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
