6f8d440b67bc092ee26d678d35ca1ef8b150c48f
musgar <abungsalman@gmail.com>
2019-06-18 13:43:06 +0700

    COA Manager Pages Built and Test

--- FinTax/FinTax.Core/Models/Coa.cs
+++ FinTax/FinTax.Core/Models/Coa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace FinTax.Core.Models
     public class Coa
     {
         public string Id { get; set; }
+
+        [DisplayName("Kode COA")]
         public string Code { get; set; }
         public string Description { get; set; }
         public string Level { get; set; }
---
+++ FinTax/FinTax.WebUI/Controllers/COAManagerController.cs
@@ -0,0 +1,119 @@
+﻿using FinTax.Core.Models;
+using FinTax.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinTax.WebUI.Controllers
+{
+    public class COAManagerController : Controller
+    {
+        CoaRepository context;
+
+        public COAManagerController()
+        {
+            context = new CoaRepository();
+        }
+
+        // GET: CoaManager
+        public ActionResult Index()
+        {
+            List<Coa> coas = context.Collection().ToList();
+            return View(coas);
+        }
+
+        public ActionResult Create()
+        {
+            Coa coa = new Coa();
+            return View(coa);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Coa coa)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(coa);
+            }
+            else
+            {
+                context.Insert(coa);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string id)
+        {
+            Coa coa = context.Find(id);
+            if (coa == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(coa);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Coa coa, string id)
+        {
+            Coa coaToEdit = context.Find(id);
+            if (coaToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(coa);
+                }
+
+                coaToEdit.Description = coa.Description;
+                coaToEdit.Code = coa.Code;
+                coaToEdit.Attachment = coa.Attachment;
+                coaToEdit.Level = coa.Level;
+                coaToEdit.ReportType = coa.ReportType;
+                coaToEdit.DownFrom = coa.DownFrom;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string id)
+        {
+            Coa coaToDelete = context.Find(id);
+            if (coaToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(coaToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string id)
+        {
+            Coa coaToDelete = context.Find(id);
+            if (coaToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(id);
+                return View(coaToDelete);
+            }
+        }
+    }
+}
